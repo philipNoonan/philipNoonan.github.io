8a48aefcfa96aaa6c1e28225feb659c400f3af0d
philNoonan <philip.noonan@outlook.com>
2020-01-21 13:22:48 +0000

    new ed

--- wglViewer/shaders/renderScreenMarchingCubes.js
+++ wglViewer/shaders/renderScreenMarchingCubes.js
@@ -2,23 +2,23 @@ const mcVertexShaderSource = `#version 310 es
     precision highp float;
     precision highp int;
 
-    layout(location = 0) in vec4 positionMC;
-    layout(location = 1) in vec4 normalMC;
+    layout(location = 0) in float positionMC;
 
     uniform float MCScaleFactor;
     uniform mat4 MVP;
+    uniform vec3 pixDims;
 
     out vec3 TexCoord3D;
-    out vec3 Normal;
 
     void main()
     {
-        //vec3 vertPoint = 128.0f + (MCScaleFactor *  vec3((positionMC & 1072693248u) >> 20u, (positionMC & 1047552u) >> 10u, positionMC & 1023u));
-        vec3 vertPoint = vec3(positionMC.xyz);
+        highp uint uData = floatBitsToUint(positionMC);
 
-        Normal = normalMC.xyz;
-        TexCoord3D = vertPoint / 256.0f;
-        gl_Position = vec4(MVP * vec4((vertPoint - vec3(256.0f)) / 32.0f, 1.0f));
+        vec3 vertPoint = vec3((uData & 1072693248u) >> 20u, (uData & 1047552u) >> 10u, uData & 1023u); // scaled between 0 - 1023
+        
+        TexCoord3D = vertPoint / 1023.0f; // scaled between 0 - 1
+        vec4 worldPoint = vec4((TexCoord3D * 2.0f - 1.0f) * pixDims, 1.0f); 
+        gl_Position = vec4(MVP * worldPoint); // scale between -1 and 1
 
     }
 `
@@ -31,7 +31,6 @@ const mcFragmentShaderSource = `#version 310 es
     
     uniform highp usampler3D volumeData;
     in vec3 TexCoord3D;
-    in vec3 Normal;
     
     out vec4 color;
     
@@ -42,7 +41,7 @@ const mcFragmentShaderSource = `#version 310 es
 // 	float ambientStrength = 0.1f;
 // 	vec3 ambient = ambientStrength * vec3(1.0f);
 // // diffuse
-vec3 norm = normalize(Normal);
+//vec3 norm = normalize(Normal);
 // 	vec3 lightDir = normalize(lightPos - FragPos);
 // 	float diff = max(dot(norm, lightDir), 0.0);
 // 	vec3 diffuse = diff * lightColor;
@@ -55,9 +54,9 @@ vec3 norm = normalize(Normal);
 
 
         //color = vec4(norm * (ambient + diffuse),1.0f);
-        
+        vec3 N = normalize(cross(dFdy(TexCoord3D), dFdx(TexCoord3D)));
         color = vec4(smoothstep(-1.0, 1.0, TexCoord3D), 1.0f);
-        //color = vec4(1.0f);
+        color = vec4(N, 1.0f);
 
     }
 `
--- wglViewer/src/render.js
+++ wglViewer/src/render.js
@@ -24,11 +24,55 @@ cosPitch * cosRoll,
 ];
 }
 
+function setOrthoVerts(x, y, z) {
+  var positions = new Float32Array([
+    1.0,	1.0,	z * 2.0 - 1.0,		// top right
+    1.0,	-1.0,	z * 2.0 - 1.0,		 // bottom right
+    -1.0,	-1.0,	z * 2.0 - 1.0,		 // bottom left
+    -1.0,	1.0,	z * 2.0 - 1.0,		  // Top left
+
+    x * 2.0 - 1.0, 1.0,	1.0,		     // top right
+    x * 2.0 - 1.0, 1.0,  -1.0,		     // bottom right
+    x * 2.0 - 1.0, -1.0, -1.0,		     // bottom left
+    x * 2.0 - 1.0, -1.0,  1.0,		      // Top left
+
+    1.0,	y * 2.0 - 1.0, 1.0,			 // top right
+    1.0,	y * 2.0 - 1.0, -1.0,		 // bottom right
+    -1.0,	y * 2.0 - 1.0, -1.0,		 // bottom left
+    -1.0,	y * 2.0 - 1.0, 1.0]);
+
+gl.bindBuffer(gl.ARRAY_BUFFER, gl.vertexPosBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
+gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+
+var texCoords = new Float32Array([
+1.0, 1.0, z, // top right
+1.0, 0.0, z, // bottom right
+0.0, 0.0, z, // bottom left
+0.0, 1.0, z, // Top left
+
+x, 1.0, 1.0, // top right
+x, 1.0, 0.0, // bottom right
+x, 0.0, 0.0, // bottom left
+x, 0.0, 1.0, // Top left
+
+1.0, y, 1.0, // top right
+1.0, y, 0.0, // bottom right
+0.0, y, 0.0, // bottom left
+0.0, y, 1.0, // Top left
+]);
+
+gl.bindBuffer(gl.ARRAY_BUFFER, gl.vertexTexBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, texCoords, gl.STATIC_DRAW);
+gl.bindBuffer(gl.ARRAY_BUFFER, null);
+}
+
 function render(gl) {
 
-gl.orientation[0] += 0.005; // yaw
-gl.orientation[1] += 0.005; // pitch
-gl.orientation[2] += 0.005; // roll
+gl.orientation[0] = gl.rotation[0]; // yaw
+gl.orientation[1] = gl.rotation[1]; // pitch
+gl.orientation[2] += 0.001; // roll
 
 
 var yawMatrix = new Float32Array(yawPitchRoll(gl.orientation[0], 0.0, 0.0));
@@ -37,13 +81,15 @@ var rollMatrix = new Float32Array(yawPitchRoll(0.0, 0.0, gl.orientation[2]));
 var yawPitchRollMatrix = new Float32Array(yawPitchRoll(gl.orientation[0], gl.orientation[1], gl.orientation[2]));
 var matrices = [yawMatrix, pitchMatrix, rollMatrix, yawPitchRollMatrix];
 
+setOrthoVerts(gl.sliderX.value / gl.size, gl.sliderY.value / gl.size, gl.sliderZ.value / gl.size);
+
 // Clear color buffer
 gl.clearColor(0.0, 0.0, 0.0, 1.0);
 gl.clear(gl.COLOR_BUFFER_BIT);
 gl.clearDepth(1.0);                 // Clear everything
 gl.enable(gl.DEPTH_TEST);           // Enable depth testing
 gl.depthFunc(gl.LEQUAL); // Near things obscure far things
-
+gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
 // Bind program
 gl.useProgram(gl.renderProgram);
 
@@ -53,10 +99,10 @@ gl.activeTexture(gl.TEXTURE0);
 gl.bindTexture(gl.TEXTURE_3D, gl.textureVolume);
 
 var translate = vec3.create();
-vec3.set(translate, 0, 0, -30);
+vec3.set(translate, gl.cameraPos[0], gl.cameraPos[1], -gl.zoom);
 var modelView = mat4.create();
 mat4.translate(modelView, modelView, translate);
-mat4.rotateX(modelView, modelView, 0.2);
+mat4.rotateX(modelView, modelView, gl.orientation[1]);
 
 
 
@@ -64,7 +110,7 @@ mat4.rotateY(modelView, modelView, gl.orientation[0]);
 
 var perspective = mat4.create();
 
-mat4.perspective(perspective, 45 * Math.PI / 180, gl.canvas.clientWidth / gl.canvas.clientHeight, 1, 100);
+mat4.perspective(perspective, 45 * Math.PI / 180, gl.canvas.clientWidth / gl.canvas.clientHeight, .01, 1000);
 
 var MVP = mat4.create();
 
@@ -82,7 +128,13 @@ gl.viewport(gl.vp.x, gl.vp.y, gl.vp.z, gl.vp.w);
 gl.uniformMatrix4fv(gl.getUniformLocation(gl.renderProgram, 'MV'), false, modelView);
 gl.uniformMatrix4fv(gl.getUniformLocation(gl.renderProgram, 'proj'), false, perspective);
 gl.uniformMatrix4fv(gl.getUniformLocation(gl.renderProgram, 'orientation'), false, matrices[0]);
-gl.drawElements(gl.TRIANGLES, 18, gl.UNSIGNED_SHORT, 0);
+gl.uniform3fv(gl.getUniformLocation(gl.renderProgram, "pixDims"), gl.pixDims);
+gl.uniform1f(gl.getUniformLocation(gl.renderProgram, "colorScale"), gl.colorScale.value);
+
+if (gl.renderOrtho.checked == true)
+{
+  gl.drawElements(gl.TRIANGLES, 18, gl.UNSIGNED_SHORT, 0);
+}
 ////gl.drawElementsInstanced(gl.TRIANGLES, 0, 18, 1);
 ////gl.drawArrays(gl.TRIANGLES, 0, 18);
 
@@ -99,10 +151,14 @@ gl.bindVertexArray(gl.vaoMarchingCubes);
 gl.uniformMatrix4fv(gl.getUniformLocation(gl.mcRenderProgram, 'MVP'), false, MVP);
 gl.uniform1f(gl.getUniformLocation(gl.mcRenderProgram, 'MCScaleFactor'), 0.25);
 gl.uniform1i(gl.getUniformLocation(gl.mcRenderProgram, 'volumeData'), 0);
+gl.uniform3fv(gl.getUniformLocation(gl.mcRenderProgram, "pixDims"), gl.pixDims);
+
 gl.activeTexture(gl.TEXTURE0);
 gl.bindTexture(gl.TEXTURE_3D, gl.textureHistoPyramid);
 
-gl.drawArrays(gl.TRIANGLES, 0, (gl.totalSumVerts));
+if (gl.renderMC.checked == true) {
+  gl.drawArrays(gl.TRIANGLES, 0, (gl.totalSumVerts));
+}
 //gl.drawArrays(gl.POINTS, 0, (gl.totalSumVerts));
 
 
