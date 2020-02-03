09028f378fa9bfffa487c23a6ff0bc2f53ec4318
philNoonan <philip.noonan@outlook.com>
2020-02-03 16:49:56 +0000

    updated flow

--- wglFlow/src/webcamera.js
+++ wglFlow/src/webcamera.js
@@ -1,48 +1,35 @@
-  class webCamera {
+  let stream;
 
-    constructor() {
+  async function getColorStream(w, h) {
+
+
+    const constraints = {
+      audio: false,
+      video: {
+        width: w,
+        height: h,
+        frameRate: {ideal: 30},
+      }
     }
 
-    static async getColorStream() {
-      if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices ||
-        !navigator.mediaDevices.getUserMedia) {
-        throw new Error("Your browser doesn't support the required mediaDevices APIs.");
-      }
-      const supported_constraints = navigator.mediaDevices.getSupportedConstraints();
-      if (supported_constraints.videoKind) {
-        let stream = await navigator.mediaDevices.getUserMedia({
-          video: {
-            videoKind: {exact: "video"},
-            frameRate: {exact: 30}
-          }
-        });
-        const track = stream.getVideoTracks()[0];
-        let settings = track.getSettings ? track.getSettings() : null;
-        return stream;
-      }
-
-      const constraints = {
-        audio: false,
-        video: {
-          width: 1280,
-          height: 720,
-          frameRate: {ideal: 30},
-        }
-      }
-
-      let stream = await navigator.mediaDevices.getUserMedia(constraints);
-      // let track = stream.getVideoTracks()[0];
-      // if (track.label.indexOf("RealSense") == -1) {
-      //   throw new Error(chromeVersion() < 58 ?
-      //     "Your browser version is too old. Get Chrome version 58 or later." :
-      //     "No RealSense camera connected.");
-      // }
-      return stream;
+    if (stream) {
+      stream.getTracks().forEach(track => {
+        track.stop();
+      });
     }
 
-}
+    stream = await navigator.mediaDevices.getUserMedia(constraints);
+    // let track = stream.getVideoTracks()[0];
+    // if (track.label.indexOf("RealSense") == -1) {
+    //   throw new Error(chromeVersion() < 58 ?
+    //     "Your browser version is too old. Get Chrome version 58 or later." :
+    //     "No RealSense camera connected.");
+    // }
+    return stream;
+  }
+
 
 function chromeVersion() {
-  const raw = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
-  return raw ? parseInt(raw[2], 10) : false;
+const raw = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
+return raw ? parseInt(raw[2], 10) : false;
 }
